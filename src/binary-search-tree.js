233d62cfb4724b9fc39a80d062abfa576d9927fb
Cat5671 <152156513+Cat5671@users.noreply.github.com>
2025-11-04 18:04:03 +0300

    fix: fix binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -39,44 +39,15 @@ class BinarySearchTree {
   remove(data) {
     if (!this.has(data)) return null;
     let removeOk = "Remove -> ";
-    this.base = removeNode(this.base, data);
+    this.root = removeNode(this.root, data);
     return removeOk;
-    /*
-                    4
-                   / \
-                  2   5
-                 / \
-                1   3
-                
-       Data remove: 3
-       1. 3 > 4 - no
-       2. 3 < 4 - yes
-       3. 4.left = 2[1,3],
-       4. 3 > 2 - yes
-       5. 2.right = [3]
-       6. 3 > 3 - no
-       7. 3 < 3 - no
-       8. 3 == 3 - yes
-       9. !3.left && !3.right - yes -> remove Node
-       Data remove: 2
-       1. 2 > 4 - no
-       2. 2 < 4 - yes
-       3. 4.left = 2[1,3],
-       4. 2 > 2 - no
-       5. 2 < 2 - no
-       6. 2 == 2 - yes
-       7. !2.left && !2.right - no
-       8. 2.left == true - yes
-       9. 2.left = [1]
-    */
+    
     function removeNode(node, data) {
       if (!node) return null; //
-      // if data > node.data, find right
       if (data > node.data) {
         node.right = removeNode(node.right, data);
         return node;
       } else if (data < node.data) {
-        // if data < node.data, find left
         node.left = removeNode(node.left, data);
         return node;
       } else {
